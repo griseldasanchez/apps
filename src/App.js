26d5d46d6b32dad4cc2a81c9a662e783ff888bbb
griseldasanchez <gs.griseldasanchez@gmail.com>
2023-11-29 21:07:12 -0800

    Working on adding components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        Griselda's Website
+      <header id="app-header">
+        Header 
       </header>
+      
+      <div id="navigation-bar">
+        Navigation Bar
+        <NavigationBar />
+      </div>
+      
+      <body id="app-body">
+        Body
+      </body>
+      
+      <footer id="app-footer">
+        Footer
+      </footer>
     </div>
   );
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NavigationBar.css";
+
+export default function NavigationBar() {
+  return (
+   <div>
+      <a href="#home">Home</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+   </div>
+  )
+}
