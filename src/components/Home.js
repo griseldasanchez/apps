6e3d2db9025ab750962b1392fa616dd43f3eda23
griseldasanchez <gs.griseldasanchez@gmail.com>
2023-11-29 20:08:14 -0800

    Removing extra files to start initial drafting

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
    <div>
-      <div id="details">Griselda's Website Details</div>
+      <div id="details">Home Page</div>
    </div>
   )
 }
